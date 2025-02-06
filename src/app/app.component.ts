cc3907cbfb8bff7eede05dfd26cc930e13c78a60
daisygith <daisygith@users.noreply.github.com>
2025-02-06 11:29:03 +0100

    added first section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'flashcards';
+
+  // constructor(public translate: TranslateService) {
+  //   translate.addLangs(['en', 'pl']);
+  //   translate.setDefaultLang('en');
+  //
+  //   const browserLang = translate.getBrowserLang();
+  //   if (browserLang != null) {
+  //     translate.use(browserLang.match(/pl|en/) ? browserLang : 'en');
+  //   }
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,21 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './shell/components/shell/shell.component';
 
 export const routes: Routes = [
-  {
-    path:'/main-page',
-    loadChildren: () =>
-      import('./main-page/main-page.routes').then((m) => m.mainPageRoutes)
-  },
   {
     path: '',
     pathMatch: 'full',
     redirectTo: '/main-page',
-  }
+  },
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'main-page',
+        loadChildren: () =>
+          import('./main-page/main-page.routes').then((m) => m.mainPageRoutes),
+      },
+    ],
+  },
 ];
