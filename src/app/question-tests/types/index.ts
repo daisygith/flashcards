a59dc47cba3381c36cc9ebc4349afe94d592254f
daisygith <daisygith@users.noreply.github.com>
2025-02-25 07:56:12 +0100

    add appearance

--- src/app/question-tests/types/index.ts
+++ src/app/question-tests/types/index.ts
@@ -1,6 +1,7 @@
 export interface QuestionTest {
   id: number;
   question: string;
+  correctAnswer: 'A' | 'B' | 'C';
   answerA: string;
   answerB: string;
   answerC: string;
--- src/app/shared/components/flashcard/flashcard.component.ts
+++ src/app/shared/components/flashcard/flashcard.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Question } from '../../../questions/types';
 import { TranslatePipe } from '@ngx-translate/core';
 import { SafeHtmlPipe } from '../../pipes/safe-html.pipe';
+import { NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-flashcard',
   standalone: true,
-  imports: [TranslatePipe, SafeHtmlPipe],
+  imports: [TranslatePipe, SafeHtmlPipe, NgbProgressbar],
   templateUrl: './flashcard.component.html',
   styleUrl: './flashcard.component.scss',
 })
